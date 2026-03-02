f236ff52611c4c255a569f3990e537b32f5500cb
Test User <test@example.com>
2026-03-02 11:55:14 +0530

    Save changes

--- addition.py
+++ addition.py
@@ -1,4 +1,4 @@
 num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+num2 = (input("Enter second number: ")) #int missing
 sum = num1 + num2
 print(f"The sum of {num1} and {num2} is {sum}")
--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print("hello yovi")
+prin("hello yovi")
